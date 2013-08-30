e579180abf3f41a2401b4d5c52c02382c5870f95
andrea <andrea@samarcanda>
2013-08-30 18:10:12 +0200

    Moved MemoryStorageService from test to src

--- broker/src/main/java/org/dna/mqtt/moquette/messaging/spi/impl/MemoryStorageService.java
+++ broker/src/main/java/org/dna/mqtt/moquette/messaging/spi/impl/MemoryStorageService.java
@@ -14,6 +14,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import org.dna.mqtt.moquette.messaging.spi.impl.HawtDBStorageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
