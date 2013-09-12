4dd633cef80c6d6f886df0311d8c49647e7b97c2
andrea <andrea@samarcanda>
2013-09-12 12:11:10 +0200

    Minor cleaned imports

--- parser/src/test/java/org/dna/mqtt/moquette/proto/PublishEncoderTest.java
+++ parser/src/test/java/org/dna/mqtt/moquette/proto/PublishEncoderTest.java
@@ -1,6 +1,5 @@
 package org.dna.mqtt.moquette.proto;
 
-import org.apache.mina.core.buffer.IoBuffer;
 import org.dna.mqtt.moquette.proto.messages.AbstractMessage.QOSType;
 import org.dna.mqtt.moquette.proto.messages.PublishMessage;
 import org.junit.Before;
