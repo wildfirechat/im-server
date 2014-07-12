aa24b19d2b3c2234e644979081dc16702a2bcc7e
selva.andre <selva.andre@gmail.com>
2014-07-12 16:30:13 +0200

    Bumped release 0.6

--- broker/src/main/java/org/dna/mqtt/moquette/server/Server.java
+++ broker/src/main/java/org/dna/mqtt/moquette/server/Server.java
@@ -41,7 +41,7 @@ public class Server {
         
         final Server server = new Server();
         server.startServer();
-        System.out.println("Server started, version 0.6-SNAPSHOT");
+        System.out.println("Server started, version 0.6");
         //Bind  a shutdown hook
         Runtime.getRuntime().addShutdownHook(new Thread() {
             @Override
