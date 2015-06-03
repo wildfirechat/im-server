47d1f94a13630d8213ecc769eacef3af7bcfcf50
andrea <selva.andre@gmail.com>
2015-06-03 22:35:28 +0200

    Added self contained client benchmarker usgin fuse, measure the complete roudtrip

---
+++ tools_scripts/benchmark/BenchmarkPublisher.groovy
@@ -0,0 +1,129 @@
+import java.net.URISyntaxException
+
+import org.fusesource.mqtt.client.BlockingConnection
+import org.fusesource.mqtt.client.CallbackConnection
+import org.fusesource.mqtt.client.MQTT
+import org.fusesource.mqtt.client.Listener
+import org.fusesource.mqtt.client.Message
+import org.fusesource.mqtt.client.QoS
+import org.fusesource.mqtt.client.Topic
+import org.fusesource.mqtt.client.Callback
+import org.fusesource.hawtbuf.Buffer
+import org.fusesource.hawtbuf.UTF8Buffer
+
+import java.nio.ByteBuffer
+import java.util.concurrent.CountDownLatch
+import org.fusesource.hawtdispatch.Task
+import org.eclipse.jetty.toolchain.perf.PlatformTimer
+
+class BenchmarkPublisher {
+    static class PublishCallback implements Callback<Void> {
+        public void onSuccess(Void value) {
+            //ok published
+            //increment counter
+        }
+
+        public void onFailure(Throwable th) {
+            println "PUB: Publish failed: " + th
+            th.printStackTrace()
+            System.exit(2)
+        }
+    }
+
+    static class ExitTopicCallback implements Callback<Void> {
+
+        private final long startedTime
+        private final long numToSend
+        private final CallbackConnection connection
+        private final CountDownLatch stopLatch
+
+        ExitTopicCallback(long startedTime, long numToSend, CallbackConnection connection, CountDownLatch stopLatch) {
+            this.startedTime = startedTime
+            this.numToSend = numToSend
+            this.connection = connection
+            this.stopLatch = stopLatch
+        }
+
+        public void onSuccess(Void value2) {
+            long stopTime = System.currentTimeMillis()
+            long spentTime = stopTime - startedTime
+            println "PUB: published in ${spentTime} ms"
+
+            this.connection.disconnect(null)
+            double msgPerSec = (numToSend / spentTime) * 1000
+            println "PUB: speed $msgPerSec msg/sec"
+            this.stopLatch.countDown()
+        }
+
+        public void onFailure(Throwable th) {
+            println "PUB: Publish failed: " + th
+            System.exit(2);
+        }
+    }
+
+    private QoS qos = QoS.AT_MOST_ONCE
+    private boolean retain = false
+    private int numToSend
+    private int messagesPerSecond
+    private String dialog_id
+    private MQTT mqtt
+    private CallbackConnection connection
+    private CountDownLatch m_latch
+
+    def connect() {
+        this.connection = mqtt.callbackConnection()
+        //connection.resume()
+        connection.connect(new Callback<Void>() {
+            public void onFailure(Throwable th) {
+                println "PUB: connect fail"
+                System.exit(2);
+            }
+
+            // Once we connect..
+            public void onSuccess(Void v) {
+                println "PUB: Succesfully connected to server"
+            }
+        })
+        this.m_latch = new CountDownLatch(1)
+    }
+
+    def firePublishes() {
+        long pauseMicroseconds = (1 / this.messagesPerSecond) * 1000 * 1000
+        println "PUB: Pause over the each message sent ${pauseMicroseconds} microsecs"
+        //byte[] message = 'Hello world!!'.bytes
+
+        print "PUB: publishing.."
+        CountDownLatch m_latch = new CountDownLatch(1)
+        final long startTime = System.currentTimeMillis()
+
+        //initialize the timer
+        PlatformTimer timer = PlatformTimer.detect()
+        def pubCallback = new PublishCallback()
+        (1..numToSend).each {
+            connection.getDispatchQueue().execute(new Task() {
+                public void run() {
+                    long nanos = System.nanoTime()
+                    byte[] message = "Hello world!!-${nanos}".bytes
+                    connection.publish("/topic" + dialog_id, message, qos, retain, pubCallback)
+                }
+            })
+            timer.sleep(pauseMicroseconds)
+        }
+
+        def exitCallback = new ExitTopicCallback(startTime, numToSend, connection, m_latch)
+        connection.getDispatchQueue().execute(new Task() {
+            public void run() {
+                long nanos = System.nanoTime()
+                byte[] message = "Now exit!!-${nanos}".bytes
+                connection.publish("/exit" + dialog_id, message, qos, retain, exitCallback)
+            }
+        })
+    }
+
+
+    def waitFinish() {
+        //wait finish...
+        m_latch.await()
+    }
+
+}
---
+++ tools_scripts/benchmark/fuse_asynch_benchmarker.groovy
@@ -0,0 +1,59 @@
+@Grab(group='org.fusesource.mqtt-client', module='mqtt-client', version='1.10')
+@Grab(group='org.eclipse.jetty.toolchain', module='jetty-perf-helper', version='1.0.5')
+
+import java.net.URISyntaxException
+
+import org.fusesource.mqtt.client.BlockingConnection
+import org.fusesource.mqtt.client.CallbackConnection
+import org.fusesource.mqtt.client.MQTT
+import org.fusesource.mqtt.client.Listener
+import org.fusesource.mqtt.client.Message
+import org.fusesource.mqtt.client.QoS
+import org.fusesource.mqtt.client.Topic
+import org.fusesource.mqtt.client.Callback
+import org.fusesource.hawtbuf.Buffer
+import org.fusesource.hawtbuf.UTF8Buffer
+
+import java.nio.ByteBuffer
+import java.util.concurrent.CountDownLatch
+import org.fusesource.hawtdispatch.Task
+import org.eclipse.jetty.toolchain.perf.PlatformTimer
+
+import BenchmarkSubscriber
+import BenchmarkPublisher
+
+if (args.size() < 3) {
+    println "Usage benchmarker <host> <num messages to sent> <frequency>[msg/sec] :[dialog_id]"
+    println "Ex localhost 10000 5000 test1"
+    println "should take 2 secs"
+    return
+}
+
+String host = args[0]
+int numToSend = args[1] as int
+int messagesPerSecond = args[2] as int
+String dialog_id = args.size() > 3 ? args[3] : ""
+
+MQTT pub = new MQTT()
+pub.setHost(host, 1883)
+pub.setCleanSession(true)
+pub.setClientId("PublisherClient${dialog_id}")
+
+MQTT sub = new MQTT()
+//mqtt.setHost("test.mosquitto.org", 1883);
+sub.setHost(host, 1883)
+sub.setCleanSession(true)
+sub.setHost(host, 1883)
+sub.setClientId("SubscriberClient${dialog_id}")
+
+BenchmarkSubscriber suscriberBench = new BenchmarkSubscriber(mqtt: sub, dialog_id: dialog_id)
+suscriberBench.connect()
+
+BenchmarkPublisher publisherBench = new BenchmarkPublisher(mqtt: pub, numToSend: numToSend, messagesPerSecond: messagesPerSecond, dialog_id: dialog_id)
+publisherBench.connect()
+publisherBench.firePublishes()
+
+suscriberBench.waitFinish()
+publisherBench.waitFinish()
+
+
