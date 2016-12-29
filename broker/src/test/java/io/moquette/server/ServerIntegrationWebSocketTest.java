ac2086a0e076846950d9e6dd0dc4f55ffe5f1694
andsel <selva.andre@gmail.com>
2016-12-30 00:31:41 +0100

    Fixed failing test due to more strict endpoint check from Netty 4.1.x

--- broker/src/test/java/io/moquette/server/ServerIntegrationWebSocketTest.java
+++ broker/src/test/java/io/moquette/server/ServerIntegrationWebSocketTest.java
@@ -71,16 +71,17 @@ public class ServerIntegrationWebSocketTest {
     @Test
     public void checkPlainConnect() throws Exception {
         LOG.info("*** checkPlainConnect ***");
-        String destUri = "ws://localhost:" + BrokerConstants.WEBSOCKET_PORT;
+        String destUri = "ws://localhost:" + BrokerConstants.WEBSOCKET_PORT + "/mqtt";
         
         MQTTWebSocket socket = new MQTTWebSocket();
         client.start();
         URI echoUri = new URI(destUri);
         ClientUpgradeRequest request = new ClientUpgradeRequest();
         client.connect(socket, echoUri, request);
-        LOG.info("Connecting to : %s", echoUri);
+        LOG.info("Connecting to : {}", echoUri);
         boolean connected = socket.awaitConnected(4, TimeUnit.SECONDS);
-        
+        LOG.info("Connected was : {}", connected);
+
         assertTrue(connected);
     }
 }
