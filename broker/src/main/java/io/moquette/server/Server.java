4162aae3a0a4e697e49983858afacb2fe1f60af5
andsel <selva.andre@gmail.com>
2017-01-11 22:06:24 +0100

    Bumped version 0.9

--- broker/src/main/java/io/moquette/server/Server.java
+++ broker/src/main/java/io/moquette/server/Server.java
@@ -68,7 +68,7 @@ public class Server {
     public static void main(String[] args) throws IOException {
         final Server server = new Server();
         server.startServer();
-        System.out.println("Server started, version 0.9-SNAPSHOT");
+        System.out.println("Server started, version 0.9");
         //Bind  a shutdown hook
         Runtime.getRuntime().addShutdownHook(new Thread() {
             @Override
--- tools_scripts/integration/brokerEmbeddedTest.groovy
+++ tools_scripts/integration/brokerEmbeddedTest.groovy
@@ -1,5 +1,5 @@
 @GrabResolver(name='moquette-bintray', root='http://dl.bintray.com/andsel/maven/')
-@Grab(group='io.moquette', module='moquette-broker', version='0.9-SNAPSHOT')
+@Grab(group='io.moquette', module='moquette-broker', version='0.9')
 
 import io.moquette.server.Server
 
