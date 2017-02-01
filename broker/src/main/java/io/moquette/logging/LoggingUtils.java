6821548d50f9af20e858ded2ef6f27673c7bca02
Andreas Mager <ama@qsc.de>
2017-02-01 15:17:16 +0100

    broker/LoggingUtils: code formatter used

--- broker/src/main/java/io/moquette/logging/LoggingUtils.java
+++ broker/src/main/java/io/moquette/logging/LoggingUtils.java
@@ -1,16 +1,17 @@
+
 package io.moquette.logging;
 
 import java.util.ArrayList;
 import java.util.Collection;
-
 import io.moquette.interception.InterceptHandler;
 
 public class LoggingUtils {
-	public static <T extends InterceptHandler> Collection<String> getInterceptorIds(Collection<T> handlers) {
-		Collection<String> result = new ArrayList<>(handlers.size());
-		for (T handler : handlers) {
-			result.add(handler.getID());
-		}
-		return result;
-	}
+
+    public static <T extends InterceptHandler> Collection<String> getInterceptorIds(Collection<T> handlers) {
+        Collection<String> result = new ArrayList<>(handlers.size());
+        for (T handler : handlers) {
+            result.add(handler.getID());
+        }
+        return result;
+    }
 }
