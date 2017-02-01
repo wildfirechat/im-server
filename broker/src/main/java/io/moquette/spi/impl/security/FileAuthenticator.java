04fdb31ef6dd110c2cb244c761b3ea6f22b52df1
Andreas Mager <ama@qsc.de>
2017-02-01 16:03:39 +0100

    broker/FileAuthenticator: code formatter used

--- broker/src/main/java/io/moquette/spi/impl/security/FileAuthenticator.java
+++ broker/src/main/java/io/moquette/spi/impl/security/FileAuthenticator.java
@@ -13,19 +13,23 @@
  *
  * You may elect to redistribute this code under either of these licenses.
  */
+
 package io.moquette.spi.impl.security;
 
 import io.moquette.server.config.FileResourceLoader;
 
 /**
- * Load user credentials from a text file.
- * Each line of the file is formatted as "[username]:[sha256(password)]". The username mustn't contains : char.
+ * Load user credentials from a text file. Each line of the file is formatted as
+ * "[username]:[sha256(password)]". The username mustn't contains : char.
  *
  * To encode your password from command line on Linux systems, you could use:
+ *
  * <pre>
  *     echo -n "yourpassword" | sha256sum
  * </pre>
- * NB -n is important because echo append a newline by default at the of string. -n avoid this behaviour.
+ *
+ * NB -n is important because echo append a newline by default at the of string. -n avoid this
+ * behaviour.
  *
  * @author andrea
  * @deprecated user {@link ResourceAuthenticator} instead
