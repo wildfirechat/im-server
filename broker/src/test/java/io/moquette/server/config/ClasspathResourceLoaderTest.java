0fc231909c027f7be6232e711e707bc7695bfc48
Andreas Mager <ama@qsc.de>
2017-02-01 16:20:21 +0100

    broker/ClasspathResourceLoaderTest: code formatter used

--- broker/src/test/java/io/moquette/server/config/ClasspathResourceLoaderTest.java
+++ broker/src/test/java/io/moquette/server/config/ClasspathResourceLoaderTest.java
@@ -1,20 +1,19 @@
+
 package io.moquette.server.config;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
-
 import io.moquette.BrokerConstants;
 
 public class ClasspathResourceLoaderTest {
 
-	@Test
-	public void testSetProperties() {
-		IResourceLoader classpathLoader = new ClasspathResourceLoader();
-		final IConfig classPathConfig = new ResourceLoaderConfig(classpathLoader);
-        assertEquals(""+BrokerConstants.PORT,classPathConfig.getProperty(BrokerConstants.PORT_PROPERTY_NAME));
+    @Test
+    public void testSetProperties() {
+        IResourceLoader classpathLoader = new ClasspathResourceLoader();
+        final IConfig classPathConfig = new ResourceLoaderConfig(classpathLoader);
+        assertEquals("" + BrokerConstants.PORT, classPathConfig.getProperty(BrokerConstants.PORT_PROPERTY_NAME));
         classPathConfig.setProperty(BrokerConstants.PORT_PROPERTY_NAME, "9999");
-        assertEquals("9999",classPathConfig.getProperty(BrokerConstants.PORT_PROPERTY_NAME));
-	}
+        assertEquals("9999", classPathConfig.getProperty(BrokerConstants.PORT_PROPERTY_NAME));
+    }
 
 }
