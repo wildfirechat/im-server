5c9d0808dd54189e1ef71008f66003a909496b21
Andreas Mager <ama@qsc.de>
2017-02-01 15:19:03 +0100

    broker/ConnectionDescriptor: code formatter used

--- broker/src/main/java/io/moquette/server/ConnectionDescriptor.java
+++ broker/src/main/java/io/moquette/server/ConnectionDescriptor.java
@@ -13,15 +13,14 @@
  *
  * You may elect to redistribute this code under either of these licenses.
  */
+
 package io.moquette.server;
 
 import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import io.moquette.server.netty.AutoFlushHandler;
 import io.moquette.server.netty.NettyUtils;
 import io.moquette.server.netty.metrics.BytesMetrics;
@@ -30,10 +29,9 @@ import io.moquette.server.netty.metrics.MessageMetrics;
 import io.moquette.server.netty.metrics.MessageMetricsHandler;
 import io.netty.channel.Channel;
 
-
 /**
- * Value object to maintain the information of single connection, like ClientID, Channel,
- * and clean session flag.
+ * Value object to maintain the information of single connection, like ClientID, Channel, and clean
+ * session flag.
  *
  *
  * @author andrea
@@ -43,10 +41,16 @@ public class ConnectionDescriptor {
     private static final Logger LOG = LoggerFactory.getLogger(ConnectionDescriptor.class);
 
     public enum ConnectionState {
-        //Connection states
-        DISCONNECTED, SENDACK, SESSION_CREATED, MESSAGES_REPUBLISHED, ESTABLISHED,
-        //Disconnection states
-        SUBSCRIPTIONS_REMOVED, MESSAGES_DROPPED, INTERCEPTORS_NOTIFIED;
+        // Connection states
+        DISCONNECTED,
+        SENDACK,
+        SESSION_CREATED,
+        MESSAGES_REPUBLISHED,
+        ESTABLISHED,
+        // Disconnection states
+        SUBSCRIPTIONS_REMOVED,
+        MESSAGES_DROPPED,
+        INTERCEPTORS_NOTIFIED;
     }
 
     public final String clientID;
@@ -59,76 +63,87 @@ public class ConnectionDescriptor {
         this.channel = session;
         this.cleanSession = cleanSession;
     }
-    
+
     public void writeAndFlush(Object payload) {
-    	this.channel.writeAndFlush(payload);
+        this.channel.writeAndFlush(payload);
     }
-    
+
     public void setupAutoFlusher(int flushIntervalMs) {
         try {
-            this.channel.pipeline().addAfter("idleEventHandler", "autoFlusher", new AutoFlushHandler(flushIntervalMs, TimeUnit.MILLISECONDS));
+            this.channel.pipeline().addAfter(
+                    "idleEventHandler",
+                    "autoFlusher",
+                    new AutoFlushHandler(flushIntervalMs, TimeUnit.MILLISECONDS));
         } catch (NoSuchElementException nseex) {
-            //the idleEventHandler is not present on the pipeline
-            this.channel.pipeline().addFirst("autoFlusher", new AutoFlushHandler(flushIntervalMs, TimeUnit.MILLISECONDS));
+            // the idleEventHandler is not present on the pipeline
+            this.channel.pipeline()
+                    .addFirst("autoFlusher", new AutoFlushHandler(flushIntervalMs, TimeUnit.MILLISECONDS));
         }
     }
-    
+
     public boolean doesNotUseChannel(Channel channel) {
-    	return !(this.channel.equals(channel));
+        return !(this.channel.equals(channel));
     }
-    
-	public boolean close() {
-		LOG.info("Closing connection descriptor. MqttClientId = {}.", clientID);
-		final boolean success = assignState(ConnectionState.INTERCEPTORS_NOTIFIED, ConnectionState.DISCONNECTED);
-		if (!success) {
-			return false;
-		}
-		this.channel.close();
-		return true;
-	}
-	
+
+    public boolean close() {
+        LOG.info("Closing connection descriptor. MqttClientId = {}.", clientID);
+        final boolean success = assignState(ConnectionState.INTERCEPTORS_NOTIFIED, ConnectionState.DISCONNECTED);
+        if (!success) {
+            return false;
+        }
+        this.channel.close();
+        return true;
+    }
+
     public String getUsername() {
-    	return NettyUtils.userName(this.channel);
+        return NettyUtils.userName(this.channel);
     }
 
     public void abort() {
-    	LOG.info("Closing connection descriptor. MqttClientId = {}.", clientID);
-//        try {
-            //this.channel.disconnect().sync();
-        this.channel.close();//.sync();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+        LOG.info("Closing connection descriptor. MqttClientId = {}.", clientID);
+        // try {
+        // this.channel.disconnect().sync();
+        this.channel.close();// .sync();
+        // } catch (InterruptedException e) {
+        // e.printStackTrace();
+        // }
     }
 
     public boolean assignState(ConnectionState expected, ConnectionState newState) {
-		LOG.debug("Updating state of connection descriptor. MqttClientId = {}, expectedState = {}, newState = {}.",
-				clientID, expected, newState);
-		boolean retval = channelState.compareAndSet(expected, newState);
-		if (!retval) {
-			LOG.error(
-					"Unable to update state of connection descriptor. MqttclientId = {}, expectedState = {}, newState = {}.",
-					clientID, expected, newState);
-		}
-		return retval;
-	}
+        LOG.debug(
+                "Updating state of connection descriptor. MqttClientId = {}, expectedState = {}, newState = {}.",
+                clientID,
+                expected,
+                newState);
+        boolean retval = channelState.compareAndSet(expected, newState);
+        if (!retval) {
+            LOG.error(
+                    "Unable to update state of connection descriptor."
+                    + " MqttclientId = {}, expectedState = {}, newState = {}.",
+                    clientID,
+                    expected,
+                    newState);
+        }
+        return retval;
+    }
 
     @Override
     public String toString() {
-        return "ConnectionDescriptor{" + "clientID=" + clientID +
-                ", cleanSession=" + cleanSession +
-                ", state=" + channelState.get() +
-                '}';
+        return "ConnectionDescriptor{" + "clientID=" + clientID + ", cleanSession=" + cleanSession + ", state="
+                + channelState.get() + '}';
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
 
         ConnectionDescriptor that = (ConnectionDescriptor) o;
 
-        if (clientID != null ? !clientID.equals(that.clientID) : that.clientID != null) return false;
+        if (clientID != null ? !clientID.equals(that.clientID) : that.clientID != null)
+            return false;
         return !(channel != null ? !channel.equals(that.channel) : that.channel != null);
 
     }
