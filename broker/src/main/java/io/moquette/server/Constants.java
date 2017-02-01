4236bc2af55bcf5ce2e7ea4f78f054e85cdc759b
Andreas Mager <ama@qsc.de>
2017-02-01 15:21:07 +0100

    broker/Constants: code formatter used

--- broker/src/main/java/io/moquette/server/Constants.java
+++ broker/src/main/java/io/moquette/server/Constants.java
@@ -13,14 +13,16 @@
  *
  * You may elect to redistribute this code under either of these licenses.
  */
+
 package io.moquette.server;
 
 /**
  * Server constants keeper
  */
 public class Constants {
+
     public static final String ATTR_CLIENTID = "ClientID";
     public static final String CLEAN_SESSION = "cleanSession";
     public static final String KEEP_ALIVE = "keepAlive";
-    public static final int MAX_MESSAGE_QUEUE = 1024; //number of messages
+    public static final int MAX_MESSAGE_QUEUE = 1024; // number of messages
 }
