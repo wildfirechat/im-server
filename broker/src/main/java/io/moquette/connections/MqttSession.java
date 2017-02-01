852a87a51685ff3d81eb89d1eaf6f19b9a9c63ad
Andreas Mager <ama@qsc.de>
2017-02-01 15:07:17 +0100

    broker/MqttSession: code formatter used

--- broker/src/main/java/io/moquette/connections/MqttSession.java
+++ broker/src/main/java/io/moquette/connections/MqttSession.java
@@ -1,56 +1,56 @@
+
 package io.moquette.connections;
 
 import java.util.Collection;
 
 /**
- * A class that represents the overall connection status of a MQTT session. Its
- * instances will be used by an external codebase when the broker is configured
- * in embedded mode.
- * 
+ * A class that represents the overall connection status of a MQTT session. Its instances will be
+ * used by an external codebase when the broker is configured in embedded mode.
+ *
  * @author lbarrios
  *
  */
 public class MqttSession {
 
-	private boolean connectionEstablished;
-	private boolean cleanSession;
-	private int inflightMessages;
-	private int pendingPublishMessagesNo;
-	private int secondPhaseAckPendingMessages;
-	private Collection<MqttSubscription> activeSubscriptions;
-	private MqttConnectionMetrics connectionMetrics;
+    private boolean connectionEstablished;
+    private boolean cleanSession;
+    private int inflightMessages;
+    private int pendingPublishMessagesNo;
+    private int secondPhaseAckPendingMessages;
+    private Collection<MqttSubscription> activeSubscriptions;
+    private MqttConnectionMetrics connectionMetrics;
 
-	public boolean isConnectionEstablished() {
-		return connectionEstablished;
-	}
+    public boolean isConnectionEstablished() {
+        return connectionEstablished;
+    }
 
-	public void setConnectionEstablished(boolean connectionEstablished) {
-		this.connectionEstablished = connectionEstablished;
-	}
+    public void setConnectionEstablished(boolean connectionEstablished) {
+        this.connectionEstablished = connectionEstablished;
+    }
 
-	public boolean isCleanSession() {
-		return cleanSession;
-	}
+    public boolean isCleanSession() {
+        return cleanSession;
+    }
 
-	public void setCleanSession(boolean cleanSession) {
-		this.cleanSession = cleanSession;
-	}
-	
-	public int getPendingPublishMessagesNo() {
+    public void setCleanSession(boolean cleanSession) {
+        this.cleanSession = cleanSession;
+    }
+
+    public int getPendingPublishMessagesNo() {
         return pendingPublishMessagesNo;
     }
-	
-	public void setPendingPublishMessagesNo(int pendingPublishMessagesNo) {
+
+    public void setPendingPublishMessagesNo(int pendingPublishMessagesNo) {
         this.pendingPublishMessagesNo = pendingPublishMessagesNo;
     }
 
-	public int getInflightMessages() {
-		return inflightMessages;
-	}
+    public int getInflightMessages() {
+        return inflightMessages;
+    }
 
-	public void setInflightMessages(int inflightMessages) {
-		this.inflightMessages = inflightMessages;
-	}
+    public void setInflightMessages(int inflightMessages) {
+        this.inflightMessages = inflightMessages;
+    }
 
     public int getSecondPhaseAckPendingMessages() {
         return secondPhaseAckPendingMessages;
