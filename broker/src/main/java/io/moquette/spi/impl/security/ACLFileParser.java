b020580a40de8ade8a415f5a380ec5da07fde765
Andreas Mager <ama@qsc.de>
2017-02-01 16:00:34 +0100

    broker/ACLFileParser: code formatter used

--- broker/src/main/java/io/moquette/spi/impl/security/ACLFileParser.java
+++ broker/src/main/java/io/moquette/spi/impl/security/ACLFileParser.java
@@ -13,29 +13,32 @@
  *
  * You may elect to redistribute this code under either of these licenses.
  */
+
 package io.moquette.spi.impl.security;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.*;
 import java.text.ParseException;
 
 /**
- * Parses the acl configuration file.
- * If a line starts with # it's comment.
- * Blank lines are skipped. The format is "topic [read|write|readwrite] {topic name}"
+ * Parses the acl configuration file. If a line starts with # it's comment. Blank lines are skipped.
+ * The format is "topic [read|write|readwrite] {topic name}"
  *
  * @author andrea
- * */
+ */
 public class ACLFileParser {
+
     private static final Logger LOG = LoggerFactory.getLogger(ACLFileParser.class);
 
     /**
      * Parse the configuration from file.
-     * @param file to parse
+     *
+     * @param file
+     *            to parse
      * @return the collector of authorizations form reader passed into.
-     * @throws ParseException if the format is not compliant.
+     * @throws ParseException
+     *             if the format is not compliant.
      */
     public static AuthorizationsCollector parse(File file) throws ParseException {
         if (file == null) {
@@ -43,27 +46,37 @@ public class ACLFileParser {
             return AuthorizationsCollector.emptyImmutableCollector();
         }
         if (!file.exists()) {
-            LOG.warn(String.format("parsing not existing file %s, so fallback on default configuration!", file.getAbsolutePath()));
+            LOG.warn(
+                    String.format(
+                            "parsing not existing file %s, so fallback on default configuration!",
+                            file.getAbsolutePath()));
             return AuthorizationsCollector.emptyImmutableCollector();
         }
         try {
             FileReader reader = new FileReader(file);
             return parse(reader);
         } catch (FileNotFoundException fex) {
-            LOG.warn(String.format("parsing not existing file %s, so fallback on default configuration!", file.getAbsolutePath()), fex);
+            LOG.warn(
+                    String.format(
+                            "parsing not existing file %s, so fallback on default configuration!",
+                            file.getAbsolutePath()),
+                    fex);
             return AuthorizationsCollector.emptyImmutableCollector();
         }
     }
 
     /**
      * Parse the ACL configuration file
-     * @param reader to parse
+     *
+     * @param reader
+     *            to parse
      * @return the collector of authorizations form reader passed into.
-     * @throws ParseException if the format is not compliant.
+     * @throws ParseException
+     *             if the format is not compliant.
      */
     public static AuthorizationsCollector parse(Reader reader) throws ParseException {
         if (reader == null) {
-            //just log and return default properties
+            // just log and return default properties
             LOG.warn("parsing NULL reader, so fallback on default configuration!");
             return AuthorizationsCollector.emptyImmutableCollector();
         }
@@ -77,15 +90,15 @@ public class ACLFileParser {
                 int commentMarker = line.indexOf('#');
                 if (commentMarker != -1) {
                     if (commentMarker == 0) {
-                        //skip its a comment
+                        // skip its a comment
                         continue;
                     } else {
-                        //it's a malformed comment
+                        // it's a malformed comment
                         throw new ParseException(line, commentMarker);
                     }
                 } else {
                     if (line.isEmpty() || line.matches("^\\s*$")) {
-                        //skip it's a black line
+                        // skip it's a black line
                         continue;
                     }
 
