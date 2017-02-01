a1dd6132327c9a9381cd761ab8a4a7e747d32036
Andreas Mager <ama@qsc.de>
2017-02-01 15:23:35 +0100

    broker/ClasspathResourceLoader: code formatter used

--- broker/src/main/java/io/moquette/server/config/ClasspathResourceLoader.java
+++ broker/src/main/java/io/moquette/server/config/ClasspathResourceLoader.java
@@ -1,16 +1,16 @@
+
 package io.moquette.server.config;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ClasspathResourceLoader implements IResourceLoader {
 
-	private static final Logger LOG = LoggerFactory.getLogger(ClasspathResourceLoader.class);
-	
+    private static final Logger LOG = LoggerFactory.getLogger(ClasspathResourceLoader.class);
+
     private final String defaultResource;
     private final ClassLoader classLoader;
 
@@ -34,7 +34,7 @@ public class ClasspathResourceLoader implements IResourceLoader {
 
     @Override
     public Reader loadResource(String relativePath) {
-    	LOG.info("Loading resource. RelativePath = {}.", relativePath);
+        LOG.info("Loading resource. RelativePath = {}.", relativePath);
         InputStream is = this.classLoader.getResourceAsStream(relativePath);
         return is != null ? new InputStreamReader(is) : null;
     }
