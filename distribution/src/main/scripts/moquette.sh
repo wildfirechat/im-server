ce0265c7e795b222a0013f6ddc9abf04f9f01a1e
Andreas Mager <andreas@mager.eu>
2017-04-14 15:21:56 +0200

    distri/moquette.sh: gc1 parameters added distri/moquette.sh: line delimeter changed to unix style

--- distribution/src/main/scripts/moquette.sh
+++ distribution/src/main/scripts/moquette.sh
@@ -12,9 +12,8 @@ echo "  | |  | | (_) | (_| | |_| |  __/ |_| ||  __/ | |  | \ \/' / | |   | |   "
 echo "  \_|  |_/\___/ \__, |\__,_|\___|\__|\__\___| \_|  |_/\_/\_\ \_/   \_/   "
 echo "                   | |                                                   "
 echo "                   |_|                                                   "
-echo "                                                                         "                                                                      
-                                                                      
-                                                             
+echo "                                                                         "
+
 
 cd "$(dirname "$0")"
 
@@ -52,5 +51,45 @@ MOQUETTE_PATH=$MOQUETTE_HOME/
 #LOG_FILE_LEVEL=fine
 JAVA_OPTS_SCRIPT="-XX:+HeapDumpOnOutOfMemoryError -Djava.awt.headless=true"
 
-$JAVA -server $JAVA_OPTS $JAVA_OPTS_SCRIPT -Dlog4j.configuration="file:$LOG_FILE" -Dmoquette.path="$MOQUETTE_PATH" -cp "$MOQUETTE_HOME/lib/*" io.moquette.server.Server
+## Use the Hotspot garbage-first collector.
+JAVA_OPTS="$JAVA_OPTS -XX:+UseG1GC"
 
+## Have the JVM do less remembered set work during STW, instead
+## preferring concurrent GC. Reduces p99.9 latency.
+JAVA_OPTS="$JAVA_OPTS -XX:G1RSetUpdatingPauseTimePercent=5"
+
+## Main G1GC tunable: lowering the pause target will lower throughput and vise versa.
+## 200ms is the JVM default and lowest viable setting
+## 1000ms increases throughput. Keep it smaller than the timeouts.
+JAVA_OPTS="$JAVA_OPTS -XX:MaxGCPauseMillis=500"
+
+## Optional G1 Settings
+
+# Save CPU time on large (>= 16GB) heaps by delaying region scanning
+# until the heap is 70% full. The default in Hotspot 8u40 is 40%.
+#JAVA_OPTS="$JAVA_OPTS -XX:InitiatingHeapOccupancyPercent=70"
+
+# For systems with > 8 cores, the default ParallelGCThreads is 5/8 the number of logical cores.
+# Otherwise equal to the number of cores when 8 or less.
+# Machines with > 10 cores should try setting these to <= full cores.
+#JAVA_OPTS="$JAVA_OPTS -XX:ParallelGCThreads=16"
+
+# By default, ConcGCThreads is 1/4 of ParallelGCThreads.
+# Setting both to the same value can reduce STW durations.
+#JAVA_OPTS="$JAVA_OPTS -XX:ConcGCThreads=16"
+
+### GC logging options -- uncomment to enable
+
+JAVA_OPTS="$JAVA_OPTS -XX:+PrintGCDetails"
+JAVA_OPTS="$JAVA_OPTS -XX:+PrintGCDateStamps"
+JAVA_OPTS="$JAVA_OPTS -XX:+PrintHeapAtGC"
+JAVA_OPTS="$JAVA_OPTS -XX:+PrintTenuringDistribution"
+JAVA_OPTS="$JAVA_OPTS -XX:+PrintGCApplicationStoppedTime"
+JAVA_OPTS="$JAVA_OPTS -XX:+PrintPromotionFailure"
+#JAVA_OPTS="$JAVA_OPTS -XX:PrintFLSStatistics=1"
+#JAVA_OPTS="$JAVA_OPTS -Xloggc:/var/log/moquette/gc.log"
+JAVA_OPTS="$JAVA_OPTS -XX:+UseGCLogFileRotation"
+JAVA_OPTS="$JAVA_OPTS -XX:NumberOfGCLogFiles=10"
+JAVA_OPTS="$JAVA_OPTS -XX:GCLogFileSize=10M"
+
+$JAVA -server $JAVA_OPTS $JAVA_OPTS_SCRIPT -Dlog4j.configuration="file:$LOG_FILE" -Dmoquette.path="$MOQUETTE_PATH" -cp "$MOQUETTE_HOME/lib/*" io.moquette.server.Server
