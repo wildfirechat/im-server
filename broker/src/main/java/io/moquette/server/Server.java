08d3e8d2782abd02f0b1fb2ef05a8a8168343c7d
andsel <selva.andre@gmail.com>
2018-05-17 23:43:33 +0200

    New dev cycle for 0.12

--- broker/src/main/java/io/moquette/server/Server.java
+++ broker/src/main/java/io/moquette/server/Server.java
@@ -75,7 +75,7 @@ public class Server {
     public static void main(String[] args) throws IOException {
         final Server server = new Server();
         server.startServer();
-        System.out.println("Server started, version 0.11");
+        System.out.println("Server started, version 0.12-SNAPSHOT");
         //Bind  a shutdown hook
         Runtime.getRuntime().addShutdownHook(new Thread(server::stopServer));
     }
--- tools_scripts/integration/brokerEmbeddedTest.groovy
+++ tools_scripts/integration/brokerEmbeddedTest.groovy
@@ -1,5 +1,5 @@
 @GrabResolver(name='moquette-bintray', root='http://dl.bintray.com/andsel/maven/')
-@Grab(group='io.moquette', module='moquette-broker', version='0.11')
+@Grab(group='io.moquette', module='moquette-broker', version='0.12-SNAPSHOT')
 
 import io.moquette.server.Server
 
