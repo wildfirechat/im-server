3080050c4dc32b92aedde1de490da7621d8b1f4b
andsel <selva.andre@gmail.com>
2018-12-01 15:10:34 +0100

    Bumped release 0.12

--- broker/src/main/java/io/moquette/broker/Server.java
+++ broker/src/main/java/io/moquette/broker/Server.java
@@ -57,7 +57,7 @@ public class Server {
     public static void main(String[] args) throws IOException {
         final Server server = new Server();
         server.startServer();
-        System.out.println("Server started, version 0.12-SNAPSHOT");
+        System.out.println("Server started, version 0.12");
         //Bind a shutdown hook
         Runtime.getRuntime().addShutdownHook(new Thread(server::stopServer));
     }
--- tools_scripts/integration/brokerEmbeddedTest.groovy
+++ tools_scripts/integration/brokerEmbeddedTest.groovy
@@ -1,5 +1,5 @@
 @GrabResolver(name='moquette-bintray', root='http://dl.bintray.com/andsel/maven/')
-@Grab(group='io.moquette', module='moquette-broker', version='0.12-SNAPSHOT')
+@Grab(group='io.moquette', module='moquette-broker', version='0.12')
 
 import io.moquette.server.Server
 
