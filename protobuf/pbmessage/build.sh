ac2d2a530aafbf3c69990f41ea3d8b1dea32fb4a
heavyrain lee <heavyrain.lee@gmail.com>
2019-04-19 08:13:43 +0800

    clean useless scripts

--- protobuf/pbmessage/build.sh
+++
@@ -1,8 +0,0 @@
-protoc -odata.pb wfcmessage.proto
-g++ -o convert convert.cpp
-./convert
-mv pbdata.h ../../../proto/mars/proto/src/Proto/
-rm convert data.pb
-protoc --java_out=./ wfcmessage.proto
-mv ./cn/wildfirechat/proto/WFCMessage.java ../../client/src/main/java/cn/wildfirechat/proto/
-rm -rf ./cn/wildfirechat/proto
