b0143bb5bf9698147c3920d96de1c92b123231c0
heavyrain lee <heavyrain.lee@gmail.com>
2019-05-02 08:05:13 +0800

    机器人信息错误

--- broker/sql/4_create_default_robot.sql
+++ broker/sql/4_create_default_robot.sql
@@ -1,3 +1,3 @@
-insert into t_user (`_uid`,`_name`,`_display_name`,`_portrait`,`_dt`) values ('FireRobot','FireRobot','小火','http://cdn2.wildfirechat.cn/robot.png',0);
+insert into t_user (`_uid`,`_name`,`_display_name`,`_portrait`,`_type`,`_dt`) values ('FireRobot','FireRobot','小火','http://cdn2.wildfirechat.cn/robot.png',1,1);
 
-insert into t_robot (`_uid`,`_owner`,`_secret`,`_callback`,`_state`,`_dt`) values ('FireRobot', 'FireRobot', '123456', 'http://192.168.0.10:8883/robot/recvmsg', 0, 0);
+insert into t_robot (`_uid`,`_owner`,`_secret`,`_callback`,`_state`,`_dt`) values ('FireRobot', 'FireRobot', '123456', 'http://localhost:8883/robot/recvmsg', 0, 1);
