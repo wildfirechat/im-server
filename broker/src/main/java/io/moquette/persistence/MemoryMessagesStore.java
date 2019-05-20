3d50b65ad973fd28221f66416027e16df2f327ff
heavyrain.lee <heavyrain.lee@gmail.com>
2019-05-20 19:34:49 +0800

    优化敏感词设置

--- broker/src/main/java/io/moquette/persistence/MemoryMessagesStore.java
+++ broker/src/main/java/io/moquette/persistence/MemoryMessagesStore.java
@@ -138,7 +138,13 @@ public class MemoryMessagesStore implements IMessagesStore {
     private void updateSensitiveWord() {
         long now = System.currentTimeMillis();
         if (now - lastUpdateSensitiveTime > 2 * 60 * 60 * 1000) {
-            lastUpdateSensitiveTime = now;
+            synchronized (this) {
+                if (now - lastUpdateSensitiveTime > 2 * 60 * 60 * 1000) {
+                    lastUpdateSensitiveTime = now;
+                } else {
+                    return;
+                }
+            }
             Set<String> sensitiveWords = databaseStore.getSensitiveWord();
             mSensitiveFilter = new SensitiveFilter(sensitiveWords);
         }
@@ -2027,6 +2033,7 @@ public class MemoryMessagesStore implements IMessagesStore {
             words) {
             databaseStore.persistSensitiveWord(word);
         }
+        lastUpdateSensitiveTime = 0;
         return true;
     }
 
