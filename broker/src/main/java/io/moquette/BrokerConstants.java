3f640b6c14cbbcbf72191a458ff5f99a764bb8cb
heavyrain lee <heavyrain.lee@gmail.com>
2019-07-21 15:11:24 +0800

    sync with commercial

--- broker/src/main/java/io/moquette/BrokerConstants.java
+++ broker/src/main/java/io/moquette/BrokerConstants.java
@@ -106,6 +106,10 @@ public final class BrokerConstants {
 
     public static final String MESSAGE_Forward_Url = "message.forward.url";
 
+    public static final String MONGODB_Client_URI = "mongodb.client_uri";
+    public static final String MONGODB_Database = "mongodb.database";
+    public static final String MONGODB_Data_Expire_Days = "mongodb.data_expire_days";
+
     public static final String MESSAGE_ROAMING = "message.roaming";
 
 
