e42d6f8e4f5829de25a1eaf29e0371452997b035
imhao183 <wildfirechat86@gmail.com>
2020-06-25 16:05:35 +0800

    fix sql error

--- broker/migrate/mysql/V36__add_group_member_count_history_message.sql
+++ broker/migrate/mysql/V36__add_group_member_count_history_message.sql
@@ -1,2 +1,2 @@
 alter table `t_group` add column `_history_message` tinyint NOT NULL DEFAULT 0, COMMENT "new group member can load group history messages";
-alter table `t_group` add column `_max_member_count` int(11) NOT NULL DEFAULT 2000, COMMENT "max group member count",
+alter table `t_group` add column `_max_member_count` int(11) NOT NULL DEFAULT 2000, COMMENT "max group member count";
