70fcc66672561b13abb6a708975c0e805e65a3b3
heavyrain2012 <heavyrain.lee@gmail.com>
2020-10-06 10:10:42 +0800

    添加默认文件传输助手用户

---
+++ broker/migrate/h2/V35__create_file_transfer_user.sql
@@ -0,0 +1,3 @@
+
+insert into t_user (`_uid`,`_name`,`_display_name`,`_portrait`,`_type`,`_dt`) values ('wfc_file_transfer','wfc_file_transfer','文件传输助手','https://static.wildfirechat.cn/wfc_file_transfer.png',1,1);
+insert into t_robot (`_uid`,`_owner`,`_secret`,`_callback`,`_state`,`_dt`) values ('wfc_file_transfer', 'wfc_file_transfer', '', '', 0, 1);
