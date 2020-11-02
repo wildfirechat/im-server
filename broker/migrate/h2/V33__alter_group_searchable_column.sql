e4588bfd9dc83558c8d4fddc5bb19550001c4951
heavyrain2012 <heavyrain.lee@gmail.com>
2020-11-02 20:18:43 +0800

    同步专业版数据库

---
+++ broker/migrate/h2/V33__alter_group_searchable_column.sql
@@ -0,0 +1,1 @@
+alter table `t_group` modify column `_searchable` int(11) NOT NULL DEFAULT 0;
---
+++ broker/migrate/mysql/V39__create_files_table.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS `t_file`;
+CREATE TABLE `t_file` (
+  `_mid` bigint(20) NOT NULL PRIMARY KEY,
+  `_from` varchar(64) NOT NULL,
+  `_type` tinyint NOT NULL DEFAULT 0,
+  `_target` varchar(64) NOT NULL,
+  `_line` int(11) NOT NULL DEFAULT 0,
+  `_name` varchar(128) DEFAULT '',
+  `_url` varchar(1024) NOT NULL DEFAULT '',
+  `_size` int(11) NOT NULL DEFAULT 0,
+  `_download_count` int(11) DEFAULT 0,
+  `_dt` bigint(20) NOT NULL,
+  INDEX `file_conv_index` (`_type`, `_line`, `_target`, `_mid`),
+  INDEX `file_user_index` (`_from`, `_mid`)
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
