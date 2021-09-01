5ce816551455dc1d8863592427a0301f56959687
heavyrain2012 <heavyrain.lee@gmail.com>
2021-09-01 13:46:57 +0800

    fix word spell

--- broker/src/main/java/win/liyufan/im/SensitiveFilter.java
+++ broker/src/main/java/win/liyufan/im/SensitiveFilter.java
@@ -31,12 +31,12 @@ public class SensitiveFilter {
             return sensitiveWords;
         }
 
-        String orignalText = text;
+        String originalText = text;
         text = text.toLowerCase();
         for(int i=0;i<text.length();i++){
             int sensitiveWordLength = getSensitiveWordLength(text, i, matchType);
             if(sensitiveWordLength>0){
-                String sensitiveWord = orignalText.substring(i, i + sensitiveWordLength);
+                String sensitiveWord = originalText.substring(i, i + sensitiveWordLength);
                 sensitiveWords.add(sensitiveWord);
                 if(matchType==MatchType.MIN_MATCH){
                     break;
