1c18d862a10bf5c93ac52f1f82ffb389c0181198
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2022-12-07 16:25:18 +0800

    会议离开加上原因

--- common/src/main/java/cn/wildfirechat/pojos/ConferenceLeaveEvent.java
+++ common/src/main/java/cn/wildfirechat/pojos/ConferenceLeaveEvent.java
@@ -3,5 +3,7 @@ package cn.wildfirechat.pojos;
 public class ConferenceLeaveEvent {
     public String userId;
     public String roomId;
+    public boolean rejoin;
+    public boolean kicked;
     public boolean screenSharing;
 }
