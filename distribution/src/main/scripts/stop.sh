9047020d69a33de4f7fd4a9e451ec07cbeaa8ce0
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2022-12-12 12:37:31 +0800

    添加停止命令

---
+++ distribution/src/main/scripts/stop.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+pid=`ps -ef | grep wildfirechat.server.Server | grep -v grep | awk '{print $2}'`
+
+if [ -z $pid ]; then
+    echo "野火IM服务不存在"
+    exit 0
+fi
+
+kill -15 $pid
+
+for i in {1..15}
+do
+  pid=`ps -ef | grep wildfirechat.server.Server | grep -v grep | awk '{print $2}'`
+  if [ -z $pid ]; then
+    echo "野火IM服务已结束"
+    exit 0
+  else
+    echo "正在结束中，请等待..."
+    sleep 1
+  fi
+done
+
+echo "正常结束失败，强制结束！"
+kill -9 $pid
